375dc634bbe776cc95a0253b5f0667c78a35dd7c
ThatIT-Guy <patilshailesh484@gmail.com>
2023-09-13 03:01:27 +0530

    demo for comparing integer values

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,5 +17,16 @@ public class Main {
                 System.out.println("a is less than b");
             }
         }
+//        we are using if else and because of that if we had to add more conditions then it will create a complex code
+//        we can use if statements only like if (a ==){} if (a>b){} and if (a<b){} but doing so will run all conditions
+//        e.g if a==b is true even then it will run if a>b and then if a<b
+//        to avoid this we have else if condition and we can add as many else ifs as we need as below
+        if (a==b) {
+            System.out.println("a is equal to b");
+        } else if (a > b) {
+            System.out.println("a is greater than b");
+        } else {
+            System.out.println("a is less than b");
+        }
     }
 }
